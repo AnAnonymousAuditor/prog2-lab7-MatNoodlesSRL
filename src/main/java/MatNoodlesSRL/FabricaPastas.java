725bce050f4b2fbf22f36b09dc7dc29e92ea4f78
ManuelBotas <manuelbotas2001@gmail.com>
2026-06-24 12:06:33 -0300

    agregada clase FabricaPastas

---
+++ src/main/java/MatNoodlesSRL/FabricaPastas.java
@@ -0,0 +1,32 @@
+package MatNoodlesSRL;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author manuel
+ */
+public class FabricaPastas {
+    private List<Pedido> pedidos;
+
+    public FabricaPastas() {
+        this.pedidos = new ArrayList<>();
+    }
+
+    public void agregarPedido() {
+
+    }
+
+    public Pedido buscarPedido(int numero){
+        return null;
+    }
+
+    public void listarPedidos() {
+
+    }
+
+    public void eliminarPedido(int numero) {
+
+    }
+}
